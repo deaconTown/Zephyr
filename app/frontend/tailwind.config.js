f5a349741794d817f1efbee1dc91a40097594e7b
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-12 16:36:45 -0500

    FrontEnd Scaffold

--- app/frontend/tailwind.config.js
+++ app/frontend/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary": "#62A87C",
+        "secondary": "#C3F3C0",
+        "accent1": "#70DBFF",
+        "accent2": "#F1EDEE",
+        "base": "#1F2421"
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
