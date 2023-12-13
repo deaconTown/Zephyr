dd848afc105c02bfadb2f4880559a47d9397524d
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-13 17:31:00 -0500

    Added Buttons for Login and Signup

--- app/frontend/src/components/Header.js
+++ app/frontend/src/components/Header.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/Zephyr Logo.png'
 import { Avatar, 
+  Button, 
   //Dropdown,
    Navbar } from 'flowbite-react';
+import LoginModal from './login_modal';
 
 const Header = () => {
+  const [ isModalOpen, setModalOpen ] = useState(false)
+  const [ selectedView, setView ] = useState("")
+
+  const handleModalClose = () => {
+    setModalOpen(false)
+  }
   return (
     <Navbar fluid rounded className='bg-secondary h-32'>
       <Navbar.Brand>
         <img src={logo} className="scale-50" alt="zephyr Logo" />
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Avatar alt="User settings" bordered />
+        {
+          <Avatar alt="User settings" bordered rounded />
+        }
         {/* <Dropdown.Header>
             <span className="block text-sm">Bonnie Green</span>
             <span className="block truncate text-sm font-medium">name@flowbite.com</span>
@@ -32,6 +42,13 @@ const Header = () => {
         <Navbar.Link className="[ text-[17px] ]" href="#">Pricing</Navbar.Link>
         <Navbar.Link className="[ text-[17px] ]" href="#">Contact</Navbar.Link>
       </Navbar.Collapse>
+      <div className="[ registration-buttons ][ flex flex-row ]">
+        <Button onClick={() => {setModalOpen(true); setView('login')}} className="[ login-btn ][ w-[150px] ][ bg-gradient-to-r from-[#1c3e35] to-[#247a4d] ]">Login</Button>
+        <Button onClick={() => {setModalOpen(true); setView('signup')}} className="[ sign-up-btn ][ w-[150px] ][ ml-2 ][ bg-gradient-to-r from-[#1c3e35] to-[#247a4d] ]">Sign Up</Button>
+        {
+          isModalOpen && < LoginModal isModalOpen={isModalOpen} initialView={selectedView} onClose={handleModalClose} />
+        }
+      </div>
     </Navbar>
   );
 }
