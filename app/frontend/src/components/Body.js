9eb297ce1e8ec060e41d5fbf89a80f81077d126a
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-14 14:29:00 -0500

    feat(dashboard-improvements) UI changes

--- app/frontend/src/components/Body.js
+++ app/frontend/src/components/Body.js
@@ -18,14 +18,14 @@ const Body = () =>{
         
     };
     return(
-        <div className='grid grid-cols-6 gap-4 mx-3 pt-10 scroll-m-1 h-screen'>
+        <div className='grid grid-cols-6 gap-4 mx-3 pt-10 h-[900px]'>
            <div className=' container p-2 space-y-6'>
             <Button gradientDuoTone='greenToBlue' className='text-base py-2 w-60' onClick={()=>handleButtonClick('Product')}>Products</Button>
             <Button gradientDuoTone='greenToBlue' className='text-base py-2 w-60' onClick={()=>handleButtonClick('Orders')}>Orders</Button>
             <Button gradientDuoTone='greenToBlue' className='text-base py-2 w-60' onClick={()=>handleButtonClick('Profile')}>Profile</Button>
            
         </div> 
-           <div className='container overflow-auto p-2 col-span-4 max-h-[500px]'>
+           <div className='container p-2 col-span-4'>
          {
          selectCard==='ProductCard' ? <ProductCard /> 
          : selectCard ==='OrdersCard' ?<OrdersCard/> 
