77069eb5cb4f982aafbbd49fcd3e7f8cfe2c0def
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-14 15:03:28 -0500

    feat(about) - WIP

---
+++ app/frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+function About(){
+    return(
+        <div className="[ about-us-section ][ h-screen ][ p-[40px] ]">
+            <div className="[ how-it-started-section ][ h-[700px] ]">
+                <h3 className="[ text-primary ]">HOW IT STARTED</h3>
+            </div>
+            
+        </div>
+    )
+}
+
+export default About;
