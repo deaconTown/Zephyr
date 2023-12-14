30272d8b95a03a084ffac12eb3e0e90104149377
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-14 15:17:54 -0500

    Added How It Started Section

--- app/frontend/src/pages/About.js
+++ app/frontend/src/pages/About.js
@@ -1,8 +1,16 @@
 function About(){
     return(
         <div className="[ about-us-section ][ h-screen ][ p-[40px] ]">
-            <div className="[ how-it-started-section ][ h-[700px] ]">
-                <h3 className="[ text-primary ]">HOW IT STARTED</h3>
+            <div className="[ how-it-started-section ][ h-auto w-[500px] ][ border-solid border-2 rounded-[10px] ][ py-[10px] px-[10px] ]">
+                <h3 className="[ text-primary font-[Roboto] text-[16px] ]">HOW IT STARTED</h3>
+                <p className="[ text-primary font-[Roboto] text-[36px] font-semibold ]">Our Dream is to Liberate People from the Frustration of Waiting in Long Lines</p>
+                <p className="[ how-it-started-par ][ mt-[20px] ]">
+                    Frustrated by the perpetual agony of waiting in never-ending lines just to grab a bite, we, like many others, yearned for a solution that didn't come with the hefty price tag of premium delivery services. We were stuck in a conundrum – the desire for efficiency without breaking the bank. The thought lingered, 'There has to be a better way.'
+                    Driven by this shared experience, our journey began. We set out to devise a solution that would eliminate the infuriating wait times without burdening our pockets. It was more than a quest for convenience; it was a rebellion against the inefficiencies that plagued our everyday experiences.
+                    Late nights brainstorming, scribbled notes on napkins, and endless cups of coffee led us to the birth of Zephyr. We envisioned a platform that would empower everyone to take charge of their time, providing a seamless way to pre-order their favorite meals and escape the shackles of queue-induced frustration.
+                    Zephyr became the manifestation of our collective desire for a smarter, more affordable alternative. It was about reclaiming control over our schedules and savoring every moment without compromise. Little did we know that our quest for a line-free world would resonate with so many, making Zephyr more than just an idea – it became a movement.
+                    From the initial spark of frustration to the creation of a game-changing platform, Zephyr is not just an innovation; it's a testament to the power of collective determination. Join us on this journey as we revolutionize the way you experience convenience, one pre-order at a time.
+                </p>
             </div>
             
         </div>
