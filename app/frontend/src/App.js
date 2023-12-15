f568990f3c1768e1d51781524a971eda9a6c96e1
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-14 20:17:01 -0500

    Added Mobile Views

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Body from './components/Body';
+import MobileHeader from './components/Mobile/MobileHeader';
+import MobileFooter from './components/Mobile/MobileFooter';
 
 function App() {
   return (
     <div className='overflow-auto overflow-x-hidden min-h-screen'>
-      <Header/>
+      <MobileHeader />
+      <Header />
       <Body/>
-      <Footer/>
+      <MobileFooter/>
+      <Footer /> 
     </div>
   );
 }
