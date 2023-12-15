95b661d7bcd83a217c8a667edcd20a642f2cc6c8
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-15 07:34:38 -0500

    Wrote Tests for Login and Registration

--- app/backend/__tests__/login.spec.js
+++ app/backend/__tests__/login.spec.js
@@ -1,13 +1,37 @@
 const {describe, expect, test} = require('@jest/globals');
+const request = require('supertest')
+const app = require('../app')
 
-describe('Login Service', () => {
+describe('Passport-Local Login', () => {
 
-  test('should login valid user', () => {
-    expect.assertions(1);
+  test('should login valid user', async() => {
+    await request(app)
+    .post('/auth/signup')
+    .send({name: 'newuser',email:'newuser@email.com',password:'test123'})
+
+    const response = await request(app)
+      .post('/auth/login')
+      .send({email:'newuser@email.com',password:'test123'})
+    
+    expect(response.statusCode).toBe(302)
   });
 
-  test('should not login invalid user', () => {
-    expect.assertions(1);
+  test('should not login user invalid with incorrect email', async() => {
+    const response = await request(app)
+      .post('/auth/login')
+      .send({email:'newr@email.com',password:'test123'})
+    
+    expect(response.statusCode).toBe(401)
+    expect(response.text).toBe('Unauthorized')
+  });
+
+  test('should not login user invalid with incorrect password', async() => {
+    const response = await request(app)
+      .post('/auth/login')
+      .send({email:'newuser@email.com',password:'test12'})
+    
+    expect(response.statusCode).toBe(401)
+    expect(response.text).toBe('Unauthorized')
   });
 
 });
