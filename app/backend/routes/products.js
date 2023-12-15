936d8534d8d007554da314f1c86238b4c654ee96
Marcal Harrison <mh614487@digicelgroup.com>
2023-12-14 22:52:15 -0500

    feat(product-service): updated product service to use prisma

--- app/backend/routes/products.js
+++ app/backend/routes/products.js
@@ -3,35 +3,44 @@ let router = express.Router();
 const ProductService = require('../services/products')
 const productService = new ProductService();
 
-router.get('/getall', function(req, res, next) {
-  res.send(productService.getAll());
+router.get('/getall', async function(req, res, next) {
+    let prods = await productService.getAll()
+  res.send(prods);
 });
 
-router.post('/createproduct', function(req, res, next){
-    productService.createProduct(req.body);
-    res.send(productService.getAll());
+router.post('/createproduct', async function(req, res, next){
+    const prod = await productService.createProduct(req.body);
+    res.send(prod);
+    //res.send(productService.getAll());
 });
 
-router.get('/:productId', function(req, res, next){
-    res.send(productService.getProduct(req.params.productId));
+router.get('/:productId', async function(req, res, next){
+    let prod = await productService.getProduct(req.params.productId)
+    res.send(prod);
 });
 
-router.put('/updateproduct', function(req, res, next){
-    productService.updateProduct(req.body);
-    res.send(productService.getAll());
+router.put('/:productId', async function(req, res, next){
+    let prod = await productService.updateProduct(req.params.productId,req.body);
+    //productService.updateProduct(req.body);
+    res.send(prod);
+    //res.send(productService.getAll());
 });
 
-router.delete('/:productId', function(req, res, next){
-    productService.deleteProduct(req.params.productId);
-    res.send(productService.getAll());
+router.delete('/:productId', async function(req, res, next){
+    let prod = await productService.deleteProduct(req.params.productId);
+    res.send(prod);
+    //res.send(productService.getAll());
 });
 
-router.get('/merchant/:merchantId', function(req, res, next){    
-    res.send(productService.getProductsByMerchant(req.params.merchantId));
+router.get('/merchant/:merchantId', async function(req, res, next){   
+    let prod = await productService.getProductsByMerchant(req.params.merchantId);
+    res.send(prod);
+    //res.send(productService.getProductsByMerchant(req.params.merchantId));
 });
 
-router.patch('/deactivate/:productId', function(req, res, next){    
-    res.send(productService.deactivateProduct(req.params.productId));
+router.patch('/deactivate/:productId', async function(req, res, next){    
+    let prod = await productService.deactivateProduct(req.params.productId);
+    res.send(prod);
 });
 
 module.exports = router;
